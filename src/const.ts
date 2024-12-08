4eb71191966f06106c1a250f995f57df73490cb4
TatsuyaInagaki <engineering20240925@gmail.com>
2024-12-08 14:27:45 +0900

    chore: 主要ライブラリの設定とRedux,Supabaseの基本的な実装
    - 主要ライブラリの設定
      - Supabase認証
      - Redux Toolkit
    - 開発環境の設定
      - ESLint
    - Reduxの認証状態管理_基本的なReduxストアの実装
    - Supabase AuthのUI実装

---
+++ src/const.ts
@@ -0,0 +1,2 @@
+export const url = import.meta.env.SUPABASE_PROJECT_URL;
+export const api = import.meta.env.SUPABASE_API_KEY;
---
+++ src/features/auth/SupabaseAuth.tsx
@@ -0,0 +1,33 @@
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import { createClient, Session } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Database } from "../../utils/database.types";
+import { api, url } from "../../const";
+
+const supabase = createClient<Database>(url, api);
+
+export default function SupabaseAuth() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
